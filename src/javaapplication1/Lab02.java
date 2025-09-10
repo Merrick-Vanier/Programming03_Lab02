2ed73dc2116cca29931524886f2e09e62537b796
6237800 <6237800@host.docker.internal>
2025-09-10 11:13:01 -0400

    Generating random image

--- src/javaapplication1/Lab02.java
+++ src/javaapplication1/Lab02.java
@@ -4,10 +4,16 @@
  */
 package javaapplication1;
 
+
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import java.util.Random;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
 
 /**
  *
@@ -25,6 +31,29 @@ public class Lab02 extends Application {
     @Override
     public void start(Stage stage) {
         BorderPane root = new BorderPane();
+        StackPane middle = new StackPane();
+        Label topLabel = new Label("Random Image");
+        Label botLabel = new Label("Voila");
+        Label lblImage = new Label("");
+        
+        Random random = new Random();
+        int rand = random.nextInt(20) + 1;
+        Image img;
+        String randStr = Integer.toString(rand);
+        if (rand < 10) {
+            img = new Image("file:images/10"+ randStr +".jpg");
+        }
+        else {
+            img = new Image("file:images/1"+ rand +".jpg");
+        }
+        
+        lblImage.setGraphic(new ImageView(img));
+        middle.getChildren().addAll(lblImage);
+        
+        root.setTop(topLabel);
+        root.setBottom(botLabel);
+        root.setCenter(middle);
+        
         Scene s = new Scene(root, 250, 300);
         stage.setTitle("Java Games");
         stage.setScene(s);
