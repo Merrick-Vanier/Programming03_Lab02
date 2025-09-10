049c02a6e8531776053a26204495a0c89a3ec668
6237800 <6237800@host.docker.internal>
2025-09-10 10:38:14 -0400

    Opens blank window

--- src/javaapplication1/Lab02.java
+++ src/javaapplication1/Lab02.java
@@ -4,17 +4,32 @@
  */
 package javaapplication1;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
 /**
  *
  * @author 6237800
  */
-public class Lab02 {
+public class Lab02 extends Application {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        launch(args);
+    }
+    
+    @Override
+    public void start(Stage stage) {
+        BorderPane root = new BorderPane();
+        Scene s = new Scene(root, 250, 300);
+        stage.setTitle("Java Games");
+        stage.setScene(s);
+        stage.show();
+
     }
     
 }
